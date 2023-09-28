61414bbe18f04de9ebcf107ea6e9f5ef27ccc506
Chance Snow <git@chancesnow.me>
2023-09-28 12:50:22 -0500

    Fix triangle shaders

--- examples/triangle/source/app.d
+++ examples/triangle/source/app.d
@@ -20,24 +20,24 @@ extern(C) void wgpu_glfw_error_callback(int error, const char* description) noth
   }
 }
 
-enum string triangleShader = `[[stage(vertex)]]
-fn vs_main([[builtin(vertex_index)]] in_vertex_index: u32) -> [[builtin(position)]] vec4<f32> {
+enum string triangleShader = `@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
   let x = f32(i32(in_vertex_index) - 1);
   let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
   return vec4<f32>(x, y, 0.0, 1.0);
 }
 
-[[stage(fragment)]]
-fn fs_main() -> [[location(0)]] vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 1.0, 1.0);
+@fragment
+fn fs_main() -> @location(0) vec4<f32> {
+  return vec4<f32>(1.0, 0.0, 0.0, 1.0);
 }`;
 enum string fullScreenQuadShader = `struct VertexOutput {
-  [[builtin(position)]] clip_position: vec4<f32>;
-  [[location(0)]] tex_coords: vec2<f32>;
+  @builtin(position) clip_position: vec4<f32>;
+  @location(0) tex_coords: vec2<f32>;
 };
 
-[[stage(vertex)]]
-fn vs_main([[builtin(vertex_index)]] in_vertex_index: u32) -> VertexOutput {
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> VertexOutput {
   var out: VertexOutput;
   let x = f32(i32(in_vertex_index) - 1);
   let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
@@ -46,13 +46,13 @@ fn vs_main([[builtin(vertex_index)]] in_vertex_index: u32) -> VertexOutput {
   return out;
 }
 
-[[group(0), binding(0)]]
+@group(0) @binding(0)
 var t_diffuse: texture_2d<f32>;
-[[group(0), binding(1)]]
+@group(0) @binding(1)
 var s_diffuse: sampler;
 
-[[stage(fragment)]]
-fn fs_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
   return textureSample(t_diffuse, s_diffuse, in.tex_coords);
 }`;
 
